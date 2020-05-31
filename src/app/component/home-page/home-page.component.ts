175c393af14b9dcfa5003bcd08d59798d4e49e4e
Balmukund SINGH <balmukund.singh241@gmail.com>
2020-05-31 09:41:17 +0530

    Ag-Grid/Basic Grid with sorting and filter done.

--- src/app/component/home-page/home-page.component.ts
+++ src/app/component/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
@@ -9,7 +9,17 @@ export class HomePageComponent implements OnInit {
 
   constructor() { }
 
+   columnDefs = [
+     {headerName: 'Make', field: 'make', sortable: true, filter: true},
+     {headerName: 'Model', field: 'model', sortable: true, filter: true},
+     {headerName: 'Price', field: 'price', sortable: true, filter: true}
+   ];
+  rowData = [
+    {make: 'Toyota', model: 'Celica', price: '35000'},
+    {make: 'Ford', model: 'Mondeo', price: '32000'},
+    {make: 'Porsche', model: 'Boxter', price: '72000'}
+  ];
+
   ngOnInit(): void {
   }
-
 }
