573cf118e498b8a1bc8b9d165ed40253969e8b78
Balmukund SINGH <balmukund.singh241@gmail.com>
2020-06-01 10:01:15 +0530

    #Feature/Static Pagination Implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import {AboutMeComponent} from './component/about-me/about-me.component';
 import {HomePageComponent} from './component/home-page/home-page.component';
 import {CommunityEditionComponent} from './component/community-edition/community-edition.component';
 import {EnterpriseEditionComponent} from './component/enterprise-edition/enterprise-edition.component';
+import {ServerSidePaginationComponent} from './component/pagination/server-side-pagination/server-side-pagination.component';
+import {StaticPaginationComponent} from './component/pagination/static-pagination/static-pagination.component';
 
 const routes: Routes = [
   { path: '',       component: HomePageComponent  },
   { path: 'community',  component: CommunityEditionComponent },
   { path: 'enterprise',  component: EnterpriseEditionComponent },
   { path: 'about',  component: AboutMeComponent },
+  { path: 'staticpagination', component: StaticPaginationComponent},
+  { path: 'communitypagination', component: ServerSidePaginationComponent},
   { path: '**',     redirectTo: '/'  }
 ];
 
